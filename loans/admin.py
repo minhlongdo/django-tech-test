d314bf956c02f42578e76beb77b96074a2751749
Minh-Long Do <minhlong94@yahoo.de>
2017-06-08 00:07:48 +0100

    Add admin site to access models in the backend

--- loans/admin.py
+++ loans/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.models import User
+
+from loans.models import Borrower, Business, Loan
+
+admin.site.unregister(User)
+admin.site.register(Borrower)
+admin.site.register(Business)
+admin.site.register(Loan)
